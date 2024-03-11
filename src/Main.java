77767b541d29892dac1711efebe6744c50612871
stonevessel <157413052+stonevessel@users.noreply.github.com>
2024-03-10 21:00:19 -0400

    yes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 class Main {
@@ -6,39 +7,118 @@ class Main {
 
     public static void main(String[] args) {
         menu();
-        ArrayList<String> myTasks = new ArrayList<>();
-        int choose = 7;
+        //ArrayList<Tasks> myTasks = new ArrayList<>();
+        HashMap<Integer, Tasks> myTasksHM= new HashMap<>();
+        int choose = -7;
         int user = 0;
 
         while (choose != 0) {
             choose = intInput();
 
             if (choose == 1) {
-                System.out.println("What task would you like to add? ");
-                String taskToAddToList = input();
-                myTasks.add(taskToAddToList);
-                menu();
+                System.out.println("Whats the priority of your task? from 1 to 5");
+                int taskPriority = intInput();
+                try{
+                    if (taskPriority <5 || taskPriority>0){
+                        System.out.println("Whats the title of your task?");
+                        String taskTitle = input();
+                        System.out.println("What task would you like to add? ");
+                        String taskToAddToList = input();
+                        Tasks task1 = new Tasks(taskPriority, taskTitle, taskToAddToList);
+                        myTasksHM.put(taskPriority,task1);
+                        menu();
+                    }else{
+                        System.out.println("try that again please enter a number between 1 and 5 for your priority");
+                        System.out.println("Whats the priority of your task? from 1 to 5");
+                        taskPriority = intInput();
+                        System.out.println("Whats the title of your task?");
+                        String taskTitle = input();
+                        System.out.println("What task would you like to add? ");
+                        String taskToAddToList = input();
+                        Tasks task1 = new Tasks(taskPriority, taskTitle, taskToAddToList);
+                        myTasksHM.put(taskPriority,task1);
+                        menu();
+                    }
+
+                }catch (Exception e){
+                    System.out.println("try that again please enter a whole number between 1 and 5 for your priority");
+                    System.out.println("Whats the priority of your task? from 1 to 5");
+                    taskPriority = intInput();
+                    System.out.println("Whats the title of your task?");
+                    String taskTitle = input();
+                    System.out.println("What task would you like to add? ");
+                    String taskToAddToList = input();
+                    Tasks task1 = new Tasks(taskPriority, taskTitle, taskToAddToList);
+                    myTasksHM.put(taskPriority,task1);
+                    menu();
+                }
             }
             if (choose == 2){
-                System.out.println(myTasks);
-                System.out.println("which task would you like to remove?");
+                System.out.println(myTasksHM);
+                System.out.println("which task would you like to remove by priority?");
                 user=intInput();
-                myTasks.remove(user);
+                System.out.println("whats the title of the task?");
+                String title = input();
+                myTasksHM.remove(user);
                 menu();
+                //come back to error proof this section olinger
             }
             if (choose == 3){
-                System.out.println(myTasks);
-                System.out.println("which task would you like to update?");
+                System.out.println(myTasksHM);
+                System.out.println("which task would you like to update by priority?");
                 user=intInput();
-                myTasks.remove(user);
-                System.out.println("Update your task.");
-                String taskToAddToList = input();
-                myTasks.add(taskToAddToList);
-                menu();
+                System.out.println("whats the title of the task?");
+                String title = input();
+                myTasksHM.remove(user);
+                System.out.println("Whats the priority of your task? from 1 to 5");
+                int taskPriority = intInput();
+                try{
+                    if (taskPriority <5 || taskPriority>0){
+                        System.out.println("Whats the title of your task?");
+                        String taskTitle = input();
+                        System.out.println("What task would you like to add? ");
+                        String taskToAddToList = input();
+                        Tasks task1 = new Tasks(taskPriority, taskTitle, taskToAddToList);
+                        myTasksHM.put(taskPriority,task1);
+                        menu();
+                    }else{
+                        System.out.println("try that again please enter a number between 1 and 5 for your priority");
+                        System.out.println("Whats the priority of your task? from 1 to 5");
+                        taskPriority = intInput();
+                        System.out.println("Whats the title of your task?");
+                        String taskTitle = input();
+                        System.out.println("What task would you like to add? ");
+                        String taskToAddToList = input();
+                        Tasks task1 = new Tasks(taskPriority, taskTitle, taskToAddToList);
+                        myTasksHM.put(taskPriority,task1);
+                        menu();
+                    }
 
+                }catch (Exception e){
+                    System.out.println("try that again please enter a whole number between 1 and 5 for your priority");
+                    System.out.println("Whats the priority of your task? from 1 to 5");
+                    taskPriority = intInput();
+                    System.out.println("Whats the title of your task?");
+                    String taskTitle = input();
+                    System.out.println("What task would you like to add? ");
+                    String taskToAddToList = input();
+                    Tasks task1 = new Tasks(taskPriority, taskTitle, taskToAddToList);
+                    myTasksHM.put(taskPriority,task1);
+                    menu();
+                }
             }
             if (choose == 4){
-                System.out.println(myTasks);
+                System.out.println(myTasksHM);
+                menu();
+            }
+            if (choose == 5){
+                System.out.println("what priority of task would you like to see?");
+                int finder = intInput();
+                System.out.println(myTasksHM.get(finder));
+                menu();
+
+
+
             }
         }
         System.out.println("System shutting down......");
@@ -51,6 +131,7 @@ class Main {
         System.out.println("(2) remove a task");
         System.out.println("(3) update a task");
         System.out.println("(4) list all tasks");
+        System.out.println("(5) list all tasks by priority");
         System.out.println("(0) quit");
     }
 
---
+++ src/Tasks.java
@@ -0,0 +1,44 @@
+public class Tasks {
+     int priority = 0;
+     String title="";
+     String task="";
+
+    public Tasks(int priority, String title, String task) {
+        this.priority = priority;
+        this.title = title;
+        this.task = task;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    @Override
+    public String toString() {
+        return "Tasks{" +
+                "priority=" + priority +
+                ", title='" + title + '\'' +
+                ", task='" + task + '\'' +
+                '}';
+    }
+}
